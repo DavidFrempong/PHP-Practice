970a50da7c03d65674d0c0533e460208d7d8cbc6
David Frempong <onlyonegraphics20@gmail.com>
2022-08-16 16:07:01 -0400

    new

---
+++ include/style.php
@@ -0,0 +1,33 @@
+<style>
+        body {
+            background: #1b1b1b;
+        }
+
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+
+        header {
+            margin: 100px 0px 0px;
+            background: #000000;
+            color: #FFFFFF;
+            text-align: center;
+        }
+
+        header a {
+            color: #FFFFFF;
+            padding: 20px;
+            display: inline-block;
+            text-decoration: none;
+        }
+
+        .active{
+           background: red;
+        }
+
+        section {
+            background: #FFFFFF;
+            min-height: 600px;
+        }
+    </style>
--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
 
+require ('include/topScripts.php');
+require ('include/style.php')
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
-<body>
-    <header>
-        <nav>
-        <a href="index.php">Home</a>
-            <a href="about.php">About</a>
-            <a href="projects.php">Projects</a>
-            <a href="contact.php">Contact</a>
-        </nav>
-    </header>
-    <section></section>
-</body>
-</html>
 
-<!-- Stopped at 2:50 -->
+<body>
+    <div class="container">
+        <?php
+            include('include/navbar.php')
+        ?>
+        <section>
+            <h1>Home</h1>
+        </section>
+    </div>
+</body>
+
+</html>
